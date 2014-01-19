9f81eac5d34ecf7621e7c06579081b764ea3a7ad
Greg Caldwell <caldwellgp@googlemail.com>
2014-01-19 20:08:38 +0000

    Depth buffer fix for mobile devices and CubeTexture implementation

--- src/flash/display3D/textures/CubeTexture.hx
+++ src/flash/display3D/textures/CubeTexture.hx
@@ -15,32 +15,54 @@ using flash.display.BitmapData;
 import flash.utils.ByteArray;
 import openfl.gl.GL;
 import openfl.gl.GLTexture;
+import openfl.utils.UInt8Array;
 
 class CubeTexture extends TextureBase 
 {
     public var size : Int;
+    public var _textures:Array<GLTexture>;
 
     public function new (glTexture:GLTexture, size : Int) {
 
         super (glTexture);
         this.size = size;
 
+        this._textures = [];
+        for( i in 0...6 )
+        {
+            this._textures[i] = GL.createTexture();
+        }
     }
 
-   public function uploadCompressedTextureFromByteArray(data:ByteArray, byteArrayOffset:Int, async:Bool = false):Void 
-   {
-      // TODO
-   }
-
-   public function uploadFromBitmapData (source:BitmapData, side:Int, miplevel:Int = 0):Void {
-
-        var p = source.getRGBAPixels();
-
-        GL.bindTexture (GL.TEXTURE_CUBE_MAP, glTexture);
+    public function uploadCompressedTextureFromByteArray(data:ByteArray, byteArrayOffset:Int, async:Bool = false):Void 
+    {
         // TODO
-        //GL.texImage2D (GL.TEXTURE_CUBE_MAP, 0, GL.RGBA, source.width, source.height, 0, GL.RGBA, GL.UNSIGNED_BYTE, new ArrayBufferView (source, 0));
+    }
 
-	}
+    public function uploadFromBitmapData( data:BitmapData, side:Int, miplevel:Int = 0 ):Void
+    {
+        var source:UInt8Array = new UInt8Array(data.getRGBAPixels());
+
+        GL.bindTexture(GL.TEXTURE_CUBE_MAP, glTexture);
+        switch( side )
+        {
+            case 0:
+                GL.texImage2D( GL.TEXTURE_CUBE_MAP_POSITIVE_X, miplevel, GL.RGBA, size, size, 0, GL.RGBA, GL.UNSIGNED_BYTE, source );
+            case 1:
+                GL.texImage2D( GL.TEXTURE_CUBE_MAP_NEGATIVE_X, miplevel, GL.RGBA, size, size, 0, GL.RGBA, GL.UNSIGNED_BYTE, source );
+            case 2:
+                GL.texImage2D( GL.TEXTURE_CUBE_MAP_POSITIVE_Y, miplevel, GL.RGBA, size, size, 0, GL.RGBA, GL.UNSIGNED_BYTE, source );
+            case 3:
+                GL.texImage2D( GL.TEXTURE_CUBE_MAP_NEGATIVE_Y, miplevel, GL.RGBA, size, size, 0, GL.RGBA, GL.UNSIGNED_BYTE, source );
+            case 4:
+                GL.texImage2D( GL.TEXTURE_CUBE_MAP_POSITIVE_Z, miplevel, GL.RGBA, size, size, 0, GL.RGBA, GL.UNSIGNED_BYTE, source );
+            case 5:
+                GL.texImage2D( GL.TEXTURE_CUBE_MAP_NEGATIVE_Z, miplevel, GL.RGBA, size, size, 0, GL.RGBA, GL.UNSIGNED_BYTE, source );
+            default :
+                throw "unknown side type";
+        }
+        GL.bindTexture( GL.TEXTURE_CUBE_MAP, null );
+    }
 
 
 	public function uploadFromByteArray(data:ByteArray, byteArrayOffset:Int, side:Int, miplevel:Int = 0):Void {
@@ -49,6 +71,10 @@ class CubeTexture extends TextureBase
 
 	}
 
+    public function glTextureAt( index:Int ):GLTexture
+    {
+        return this._textures[ index ];
+    }
 }
 
 #end
